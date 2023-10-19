f57b1de6e272f667f0502d4db1b72174f8b2471b
cosmicintruder <littlegreenbagg@googlemail.com>
2023-10-19 17:04:18 +0400

    change mod settings / change main

--- main.go
+++ main.go
@@ -5,17 +5,5 @@ import (
 )
 
 func main() {
-	// logLevel := "info"
-	// logFilePath := "logger/logs/log.json"
-
-	// if err := logger.Initialize(logLevel, logFilePath); err != nil {
-	// 	panic("Failed to initialize logger: " + err.Error())
-	// }
-
-	// mylog := logger.Get()
-
-	// mylog.Info("This is an info log message")
-	// mylog.Error("This is an error log message", zap.Error(errors.New("test")))
-
 	cmd.Execute()
 }
