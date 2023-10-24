66704d54cf6491c3f81135740c0450c6f2b24c13
cosmicintruder <littlegreenbagg@googlemail.com>
2023-10-24 16:37:02 +0400

    added register handler

---
+++ services/auth/api_auth.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	internalErr "projects/fb-server/errors"
+	"projects/fb-server/pkg/httplib"
+	"projects/fb-server/pkg/model"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func (s *service) Register(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	decoder := json.NewDecoder(r.Body)
+	var req model.RegisterRequest
+	if err := decoder.Decode(&req); err != nil {
+		httplib.ErrorResponseJSON(w, http.StatusBadRequest, internalErr.AuthDecode, err)
+	}
+
+	if !req.TermsOk {
+		httplib.ErrorResponseJSON(w, http.StatusBadRequest, internalErr.AuthForm,
+			fmt.Errorf("you must accept terms and contiditons 'terms_ok' set to true"))
+		return
+	}
+
+	tx, err := s.Repo.Pool.BeginTx(ctx, pgx.TxOptions{
+		IsoLevel: pgx.Serializable,
+	})
+	if err != nil {
+		s.Logger.Errorf("Unable to begin transaction: %s", err)
+		httplib.ErrorResponseJSON(w, http.StatusBadRequest, internalErr.Tx, err)
+	}
+
+	credentials, err := s.createUserCredentials(ctx, tx, &req)
+
+	if txErr := tx.Commit(ctx); txErr != nil {
+		s.Logger.Errorf("Unable to commit transaction: %s", txErr)
+		httplib.ErrorResponseJSON(w, http.StatusBadRequest, internalErr.TxCommit, txErr)
+		return
+	}
+
+	result := httplib.SuccessfulResult()
+	result.Id = credentials.UserId
+
+	httplib.ResponseJSON(w, result)
+}
+
+func (s *service) Login(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ services/auth/auth_utils.go
@@ -0,0 +1,96 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+	"net/mail"
+	internalErr "projects/fb-server/errors"
+	"projects/fb-server/pkg/httplib"
+	"projects/fb-server/pkg/model"
+	"projects/fb-server/pkg/utils"
+	"strings"
+	"time"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5"
+	"github.com/spf13/viper"
+)
+
+const (
+	saltLength = 32
+)
+
+func (s service) createUserCredentials(ctx context.Context, tx pgx.Tx, req *model.RegisterRequest) (*model.UserCredentials, error) {
+	if req.Email == "" {
+		intErr := internalErr.New(internalErr.AuthFormEmailInvalid)
+		return nil, httplib.NewApiErrFromInternalErr(intErr)
+	}
+
+	req.Email = strings.ToLower(req.Email)
+
+	if _, err := mail.ParseAddress(req.Email); err != nil {
+		intErr := internalErr.New(internalErr.AuthFormEmailInvalid)
+		return nil, httplib.NewApiErrFromInternalErr(intErr)
+	}
+
+	if req.Password == "" || len(req.Password) < 6 {
+		intErr := internalErr.New(internalErr.AuthFormPasswordInvalid)
+		return nil, httplib.NewApiErrFromInternalErr(intErr)
+	}
+
+	user := model.User{
+		Name:      req.Name,
+		CreatedAt: time.Now().Unix(),
+	}
+
+	userId, err := s.Repo.TxCreateUser(ctx, tx, user)
+	if err != nil {
+		if txErr := tx.Rollback(ctx); txErr != nil {
+			s.Logger.Errorf("Unable to rollback transaction: %s", txErr)
+		}
+		if err.(*pgconn.PgError).Code == pgerrcode.UniqueViolation {
+			intErr := internalErr.New(internalErr.TxNotUnique)
+			return nil, httplib.NewApiErrFromInternalErr(intErr)
+		} else {
+			intErr := internalErr.New(internalErr.TxUnknown)
+			s.Logger.Errorf("Failed to create user during registration transaction: %s", err)
+			return nil, httplib.NewApiErrFromInternalErr(intErr, http.StatusInternalServerError)
+		}
+	}
+
+	salt := utils.GetRandomString(saltLength)
+	password := utils.GenerateSaltedHash(req.Password, salt)
+
+	activationDisabled := !viper.GetBool("auth.require_verification")
+
+	userCredentials := model.UserCredentials{
+		UserId:   userId,
+		Email:    req.Email,
+		Password: password,
+		Salt:     salt,
+		Active:   activationDisabled,
+	}
+
+	if !activationDisabled {
+		userCredentials.Token = utils.GenerateHashFromString(req.Email + password + salt + req.Name)
+		userCredentials.TokenExpire = time.Now().Unix() + 60*60*48
+		userCredentials.TokenType = model.TokenConfirmation
+	}
+
+	if err := s.Repo.TxNewAuthCredentials(ctx, tx, userCredentials); err != nil {
+		if txErr := tx.Rollback(ctx); txErr != nil {
+			s.Logger.Errorf("Unable to rollback transaction: %s", txErr)
+		}
+		if err.(*pgconn.PgError).Code == pgerrcode.UniqueViolation {
+			intErr := internalErr.New(internalErr.TxNotUnique)
+			return nil, httplib.NewApiErrFromInternalErr(intErr)
+		} else {
+			intErr := internalErr.New(internalErr.TxUnknown)
+			s.Logger.Errorf("Failed to create user during registration transaction: %s", err)
+			return nil, httplib.NewApiErrFromInternalErr(intErr, http.StatusInternalServerError)
+		}
+	}
+
+	return &userCredentials, nil
+}
