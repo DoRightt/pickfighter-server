b03904ea4780f3f1451ef32275b5832252ec8971
cosmicintruder <littlegreenbagg@googlemail.com>
2023-11-06 18:18:29 +0400

    added find and confirm credentials methods

--- repo/auth/pgx_credentials.go
+++ repo/auth/pgx_credentials.go
@@ -2,8 +2,10 @@ package repo
 
 import (
 	"context"
+	"fmt"
 	"projects/fb-server/pkg/model"
 
+	"github.com/jackc/pgtype"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -29,5 +31,61 @@ func (r *AuthRepo) TxNewAuthCredentials(ctx context.Context, tx pgx.Tx, uc model
 	}
 
 	return nil
+}
 
+func (r *AuthRepo) FindUserCredentials(ctx context.Context, req model.UserCredentialsRequest) (model.UserCredentials, error) {
+	q := `SELECT user_id, email, password_hash, salt, token, token_type, token_expire, active
+		FROM public.fb_user_credentials`
+
+	if req.Email != "" {
+		q += ` WHERE email = '` + r.SanitizeString(req.Email) + `'`
+	} else if req.Token != "" {
+		q += ` WHERE token = '` + r.SanitizeString(req.Token) + `'`
+	} else if req.UserId > 0 {
+		q += fmt.Sprintf(` WHERE user_id = %d`, req.UserId)
+	} else {
+		return model.UserCredentials{}, fmt.Errorf("no query parameters")
+	}
+
+	var c model.UserCredentials
+	var currentToken, tokenType pgtype.Varchar
+	var tokenExpire pgtype.Int8
+
+	err := r.Pool.QueryRow(ctx, q).Scan(&c.UserId, &c.Email, &c.Password, &c.Salt, &currentToken, &tokenType, &tokenExpire, &c.Active)
+	if err != nil {
+		return c, r.DebugLogSqlErr(q, err)
+	}
+
+	c.Token = currentToken.String
+	c.TokenType = currentToken.String
+	c.TokenExpire = tokenExpire.Int
+
+	return c, nil
+}
+
+func (r *AuthRepo) ConfirmCredentialsToken(ctx context.Context, tx pgx.Tx, req model.UserCredentialsRequest) error {
+	q := `UPDATE public.fb_user_credentials
+		SET active = true, token = $2, token_type = $3, token_expire = NULL
+		WHERE user_id = $1`
+
+	var token, tokenType *string
+	if len(req.Token) > 0 {
+		token = &req.Token
+	}
+	if len(req.TokenType) > 0 {
+		tokenType = &req.TokenType
+	}
+
+	args := []any{req.UserId, token, tokenType}
+	if tx != nil {
+		if _, err := tx.Exec(ctx, q, args...); err != nil {
+			return r.DebugLogSqlErr(q, err)
+		}
+	} else {
+		if _, err := r.Pool.Exec(ctx, q, args...); err != nil {
+			return r.DebugLogSqlErr(q, err)
+		}
+	}
+
+	return nil
 }
