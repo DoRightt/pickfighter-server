2b298bce8cdf99317788295911fa87c2800e66d1
cosmicintruder <littlegreenbagg@googlemail.com>
2023-11-08 19:04:15 +0400

    added ifloggedin middleware

---
+++ services/middlewares.go
@@ -0,0 +1,78 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"projects/fb-server/pkg/httplib"
+	"projects/fb-server/pkg/model"
+	"time"
+
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+	"github.com/spf13/viper"
+)
+
+func (h *ApiHandler) verifyJWT(jwtRawValue string) (jwt.Token, error) {
+	alg := jwa.RS256
+
+	token, err := jwt.Parse([]byte(jwtRawValue), jwt.WithKey(alg, viper.Get("auth.jwt.parse_key")))
+	if err != nil {
+		h.Logger.Debugf("Failed to parse JWT token: %s", err)
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func (h *ApiHandler) IfLoggedIn(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		cookie, err := r.Cookie(httplib.CookieName)
+		if err != nil {
+			h.Logger.Debugf("access token not found: %s", err)
+			httplib.ErrorResponseJSON(w, http.StatusUnauthorized, http.StatusUnauthorized,
+				fmt.Errorf("unauthorized request: auth cookie or headers not found"))
+			return
+		}
+
+		token, err := h.verifyJWT(cookie.Value)
+		if err != nil {
+			h.Logger.Debugf("Failed to parse JWT token: %s", err)
+			httplib.ErrorResponseJSON(w, http.StatusUnauthorized, http.StatusUnauthorized,
+				fmt.Errorf("unauthorized request: invalid token format"))
+			return
+		}
+
+		userId, ok := token.Get(model.ContextUserId)
+		if !ok {
+			httplib.ErrorResponseJSON(w, http.StatusUnauthorized, http.StatusUnauthorized,
+				fmt.Errorf("illegal token, user id must be specified"))
+			return
+		}
+
+		if token.Expiration().Unix() < time.Now().Unix() {
+			httplib.ErrorResponseJSON(w, http.StatusUnauthorized, http.StatusUnauthorized,
+				fmt.Errorf("token expired"))
+			return
+		}
+
+		uid, valid := userId.(float64)
+		if valid {
+			ctx = context.WithValue(ctx, model.ContextUserId, int32(uid))
+		}
+
+		rootClaim, onBoard := token.Get(model.ContextClaim)
+		if onBoard {
+			claim, fit := rootClaim.(string)
+			if fit {
+				ctx = context.WithValue(ctx, model.ContextNamespaceClaims, claim)
+			}
+		}
+
+		ctx = context.WithValue(ctx, model.ContextJWTPointer, token)
+
+		fn(w, r.WithContext(ctx))
+	}
+}
