8db132212b770854128b199bf10b8c891d8b55d8
cosmicintruder <littlegreenbagg@googlemail.com>
2023-11-21 16:15:27 +0400

    added create and update fighters method for sql

--- pkg/scraper/utils.go
+++ pkg/scraper/utils.go
@@ -3,12 +3,18 @@ package scraper
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
+	internalErr "projects/fb-server/errors"
 	"projects/fb-server/pkg/cfg"
+	"projects/fb-server/pkg/httplib"
 	"projects/fb-server/pkg/model"
 	"projects/fb-server/pkg/pgxs"
 	fighterRepo "projects/fb-server/repo/fighters"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
 	"go.uber.org/zap"
 )
@@ -40,16 +46,110 @@ func WriteFighterData(ctx context.Context, l *zap.SugaredLogger, data []model.Fi
 	defer db.Pool.Close()
 
 	rep := fighterRepo.New(db)
+	counter := 1
 
+	for _, fighter := range data {
+		fighterId, err := rep.FindFighter(ctx, fighter)
+		if err != nil {
+			if err == pgx.ErrNoRows {
+				if err := createFighterTx(ctx, rep, fighter); err != nil {
+					l.Errorf("Error while fighter transaction: %s", err)
+					return err
+				}
+
+				fmt.Printf("[Operation №%d] Created: %s\n", counter, fighter.Name)
+			} else {
+				l.Errorf("Failed to find fighter: %s", err)
+				return err
+			}
+		} else {
+			fighterReq := model.FighterReq{
+				FighterId: fighterId,
+				Fighter:   &fighter,
+			}
+
+			if err := updateFighterTx(ctx, rep, fighterReq); err != nil {
+				l.Errorf("Error while fighter transaction: %s", err)
+				return err
+			}
+
+			fmt.Printf("[Operation №%d] Updated: %s\n", counter, fighter.Name)
+		}
+		counter += 1
+	}
+
+	return nil
+}
+
+func createFighterTx(ctx context.Context, rep *fighterRepo.FighterRepo, fighter model.Fighter) error {
 	tx, err := rep.Pool.BeginTx(ctx, pgx.TxOptions{
-		IsoLevel: pgx.Serializable,
+		IsoLevel: pgx.ReadCommitted,
 	})
 	if err != nil {
 		l.Errorf("Unable to begin transaction: %s", err)
 	}
 	defer tx.Rollback(ctx)
 
-	err = rep.InsertFightersData(ctx, tx, data)
+	fighterId, err := rep.CreateNewFighter(ctx, tx, fighter)
+	if err != nil {
+		if txErr := tx.Rollback(ctx); txErr != nil {
+			l.Errorf("Unable to rollback transaction: %s", txErr)
+		}
+		if err.(*pgconn.PgError).Code == pgerrcode.UniqueViolation {
+			intErr := internalErr.New(internalErr.TxNotUnique)
+			return httplib.NewApiErrFromInternalErr(intErr)
+		} else {
+			intErr := internalErr.New(internalErr.TxUnknown)
+			l.Errorf("Failed to create fighter during registration transaction: %s", err)
+			return httplib.NewApiErrFromInternalErr(intErr, http.StatusInternalServerError)
+		}
+	}
+	stats := model.FighterStatsReq{
+		FighterStats: &fighter.Stats,
+		FighterId:    fighterId,
+	}
+	rep.CreateNewFighterStats(ctx, tx, stats)
+
+	if txErr := tx.Commit(ctx); txErr != nil {
+		l.Errorf("Unable to commit transaction: %s", txErr)
+		return txErr
+	}
+
+	return nil
+}
+
+func updateFighterTx(ctx context.Context, rep *fighterRepo.FighterRepo, fighter model.FighterReq) error {
+	tx, err := rep.Pool.BeginTx(ctx, pgx.TxOptions{
+		IsoLevel: pgx.ReadCommitted,
+	})
+	if err != nil {
+		l.Errorf("Unable to begin transaction: %s", err)
+	}
+	defer tx.Rollback(ctx)
+
+	updatedId, err := rep.UpdateFighter(ctx, tx, fighter)
+	if err != nil {
+		if txErr := tx.Rollback(ctx); txErr != nil {
+			l.Errorf("Unable to rollback transaction: %s", txErr)
+		}
+		if err.(*pgconn.PgError).Code == pgerrcode.UniqueViolation {
+			intErr := internalErr.New(internalErr.TxNotUnique)
+			return httplib.NewApiErrFromInternalErr(intErr)
+		} else {
+			intErr := internalErr.New(internalErr.TxUnknown)
+			l.Errorf("Failed to update fighter during registration transaction: %s", err)
+			return httplib.NewApiErrFromInternalErr(intErr, http.StatusInternalServerError)
+		}
+	}
+
+	stats := model.FighterStatsReq{
+		FighterStats: &fighter.Stats,
+		FighterId:    updatedId,
+	}
+
+	if err := rep.UpdateFighterStats(ctx, tx, stats); err != nil {
+		l.Errorf("Error while updating stats: %s", err)
+	}
 
 	if txErr := tx.Commit(ctx); txErr != nil {
 		l.Errorf("Unable to commit transaction: %s", txErr)
--- repo/fighters/pgx_fighters.go
+++ repo/fighters/pgx_fighters.go
@@ -7,70 +7,137 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func (r *FighterRepo) InsertFightersData(ctx context.Context, tx pgx.Tx, fighters []model.Fighter) error {
-	qData := `INSERT INTO fb_fighters (
+func (r *FighterRepo) FindFighter(ctx context.Context, req model.Fighter) (int32, error) {
+	q := `SELECT fighter_id FROM fb_fighters WHERE name = $1 AND debut_timestamp = $2`
+	var fighterId int32
+
+	err := r.Pool.QueryRow(ctx, q, req.Name, req.DebutTimestamp).Scan(&fighterId)
+	if err != nil {
+		return fighterId, err
+	}
+
+	return fighterId, nil
+}
+
+func (r *FighterRepo) CreateNewFighter(ctx context.Context, tx pgx.Tx, fighter model.Fighter) (int32, error) {
+	qData := `INSERT INTO public.fb_fighters (
 		name, nickname, division, status, hometown,
 		trains_at, fighting_style, age, height, weight,
 		octagon_debut, debut_timestamp, reach, leg_reach, wins,
 		loses, draw, fighter_url, image_url
-	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`
+	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)
+	RETURNING fighter_id`
 
-	qStats := `INSERT INTO fb_fighter_stats (
-		total_sig_str_landed, total_sig_str_attempted, str_accuracy, total_tkd_landed, total_tkd_attempted,
-		tkd_accuracy, sig_str_landed, sig_str_absorbed, sig_str_defense, takedown_defense,
-		takedown_avg, submission_avg, knockdown_avg, avg_fight_time, win_by_ko,
-		win_by_sub, win_by_dec
-	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)`
+	var fighterId int32
 
-	for _, fighter := range fighters {
-		if tx != nil {
-			_, err := tx.Exec(ctx,
-				qData,
-				fighter.Name, fighter.NickName, fighter.Division, fighter.Status, fighter.Hometown,
-				fighter.TrainsAt, fighter.FightingStyle, fighter.Age, fighter.Height, fighter.Weight,
-				fighter.OctagonDebut, fighter.DebutTimestamp, fighter.Reach, fighter.LegReach, fighter.Wins,
-				fighter.Loses, fighter.Draw, fighter.FighterUrl, fighter.ImageUrl,
-			)
-			if err != nil {
-				return err
-			}
-		} else {
-			_, err := r.Pool.Exec(ctx,
-				qData,
-				fighter.Name, fighter.NickName, fighter.Division, fighter.Status, fighter.Hometown,
-				fighter.TrainsAt, fighter.FightingStyle, fighter.Age, fighter.Height, fighter.Weight,
-				fighter.OctagonDebut, fighter.DebutTimestamp, fighter.Reach, fighter.LegReach, fighter.Wins,
-				fighter.Loses, fighter.Draw, fighter.FighterUrl, fighter.ImageUrl,
-			)
-			if err != nil {
-				return err
-			}
+	args := []any{
+		fighter.Name, fighter.NickName, fighter.Division, fighter.Status, fighter.Hometown,
+		fighter.TrainsAt, fighter.FightingStyle, fighter.Age, fighter.Height, fighter.Weight,
+		fighter.OctagonDebut, fighter.DebutTimestamp, fighter.Reach, fighter.LegReach, fighter.Wins,
+		fighter.Loses, fighter.Draw, fighter.FighterUrl, fighter.ImageUrl,
+	}
+
+	if tx != nil {
+		if err := tx.QueryRow(ctx, qData, args...).Scan(&fighterId); err != nil {
+			return 0, r.DebugLogSqlErr(qData, err)
 		}
-
-		if tx != nil {
-			_, err := tx.Exec(ctx,
-				qStats,
-				fighter.Stats.TotalSigStrLanded, fighter.Stats.TotalSigStrAttempted, fighter.Stats.StrAccuracy, fighter.Stats.TotalTkdLanded, fighter.Stats.TotalTkdAttempted,
-				fighter.Stats.TkdAccuracy, fighter.Stats.SigStrLanded, fighter.Stats.SigStrAbs, fighter.Stats.SigStrDefense, fighter.Stats.TakedownDefense,
-				fighter.Stats.TakedownAvg, fighter.Stats.SubmissionAvg, fighter.Stats.KnockdownAvg, fighter.Stats.AvgFightTime, fighter.Stats.WinByKO,
-				fighter.Stats.WinBySub, fighter.Stats.WinByDec,
-			)
-			if err != nil {
-				return err
-			}
-		} else {
-			_, err := r.Pool.Exec(ctx,
-				qStats,
-				fighter.Stats.TotalSigStrLanded, fighter.Stats.TotalSigStrAttempted, fighter.Stats.StrAccuracy, fighter.Stats.TotalTkdLanded, fighter.Stats.TotalTkdAttempted,
-				fighter.Stats.TkdAccuracy, fighter.Stats.SigStrLanded, fighter.Stats.SigStrAbs, fighter.Stats.SigStrDefense, fighter.Stats.TakedownDefense,
-				fighter.Stats.TakedownAvg, fighter.Stats.SubmissionAvg, fighter.Stats.KnockdownAvg, fighter.Stats.AvgFightTime, fighter.Stats.WinByKO,
-				fighter.Stats.WinBySub, fighter.Stats.WinByDec,
-			)
-			if err != nil {
-				return err
-			}
+	} else {
+		if err := r.Pool.QueryRow(ctx, qData, args...).Scan(&fighterId); err != nil {
+			return 0, r.DebugLogSqlErr(qData, err)
 		}
+	}
 
+	return fighterId, nil
+}
+
+func (r *FighterRepo) CreateNewFighterStats(ctx context.Context, tx pgx.Tx, stats model.FighterStatsReq) error {
+	qStats := `INSERT INTO public.fb_fighter_stats (
+		fighter_id, total_sig_str_landed, total_sig_str_attempted, str_accuracy, total_tkd_landed, 
+		total_tkd_attempted, tkd_accuracy, sig_str_landed, sig_str_absorbed, sig_str_defense,
+		takedown_defense, takedown_avg, submission_avg, knockdown_avg, avg_fight_time,
+		win_by_ko, win_by_sub, win_by_dec
+	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)`
+
+	args := []any{
+		stats.FighterId, stats.TotalSigStrLanded, stats.TotalSigStrAttempted, stats.StrAccuracy, stats.TotalTkdLanded,
+		stats.TotalTkdAttempted, stats.TkdAccuracy, stats.SigStrLanded, stats.SigStrAbs, stats.SigStrDefense,
+		stats.TakedownDefense, stats.TakedownAvg, stats.SubmissionAvg, stats.KnockdownAvg, stats.AvgFightTime,
+		stats.WinByKO, stats.WinBySub, stats.WinByDec,
+	}
+
+	if tx != nil {
+		_, err := tx.Exec(ctx, qStats, args...)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := r.Pool.Exec(ctx, qStats, args...)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *FighterRepo) UpdateFighter(ctx context.Context, tx pgx.Tx, fighter model.FighterReq) (int32, error) {
+	qData := `UPDATE public.fb_fighters SET
+		nickname = $2, division = $3, status = $4, hometown = $5, trains_at = $6, 
+		fighting_style = $7, age = $8, height = $9, weight = $10, octagon_debut = $11, 
+		debut_timestamp = $12, reach = $13, leg_reach = $14, wins = $15, loses = $16,
+		draw = $17, fighter_url = $18, image_url = $19
+		WHERE fighter_id = $1
+		RETURNING fighter_id`
+
+	var fighterId int32
+
+	args := []any{
+		fighter.FighterId,
+		fighter.NickName, fighter.Division, fighter.Status, fighter.Hometown, fighter.TrainsAt,
+		fighter.FightingStyle, fighter.Age, fighter.Height, fighter.Weight, fighter.OctagonDebut,
+		fighter.DebutTimestamp, fighter.Reach, fighter.LegReach, fighter.Wins, fighter.Loses,
+		fighter.Draw, fighter.FighterUrl, fighter.ImageUrl,
+	}
+
+	if tx != nil {
+		if err := tx.QueryRow(ctx, qData, args...).Scan(&fighterId); err != nil {
+			return 0, r.DebugLogSqlErr(qData, err)
+		}
+	} else {
+		if err := r.Pool.QueryRow(ctx, qData, args...).Scan(&fighterId); err != nil {
+			return 0, r.DebugLogSqlErr(qData, err)
+		}
+	}
+
+	return fighterId, nil
+}
+
+func (r *FighterRepo) UpdateFighterStats(ctx context.Context, tx pgx.Tx, stats model.FighterStatsReq) error {
+	qStats := `UPDATE public.fb_fighter_stats SET
+		total_sig_str_landed = $2, total_sig_str_attempted = $3, str_accuracy = $4, total_tkd_landed = $5, total_tkd_attempted = $6, 
+		tkd_accuracy = $7, sig_str_landed = $8, sig_str_absorbed = $9, sig_str_defense = $10, takedown_defense = $11, 
+		takedown_avg = $12, submission_avg = $13, knockdown_avg = $14, avg_fight_time = $15, win_by_ko = $16, 
+		win_by_sub = $17, win_by_dec = $18
+		WHERE fighter_id = $1`
+
+	args := []any{
+		stats.FighterId,
+		stats.TotalSigStrLanded, stats.TotalSigStrAttempted, stats.StrAccuracy, stats.TotalTkdLanded, stats.TotalTkdAttempted,
+		stats.TkdAccuracy, stats.SigStrLanded, stats.SigStrAbs, stats.SigStrDefense, stats.TakedownDefense,
+		stats.TakedownAvg, stats.SubmissionAvg, stats.KnockdownAvg, stats.AvgFightTime, stats.WinByKO,
+		stats.WinBySub, stats.WinByDec,
+	}
+
+	if tx != nil {
+		_, err := tx.Exec(ctx, qStats, args...)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := r.Pool.Exec(ctx, qStats, args...)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
