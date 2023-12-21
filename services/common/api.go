eca2f305a6566545d937df7ee61d350ce740f402
cosmicintruder <littlegreenbagg@googlemail.com>
2023-12-21 18:48:33 +0400

    changes create/bet route handler

--- services/common/api.go
+++ services/common/api.go
@@ -34,7 +34,7 @@ func (s *service) ApplyRoutes() {
 	s.Router.HandleFunc("/create/event", s.CheckIsAdmin(s.HandleNewEvent)).Methods(http.MethodPost)
 	s.Router.HandleFunc("/events", s.GetEvents).Methods(http.MethodGet)
 
-	s.Router.HandleFunc("/create/bet", s.CheckIsAdmin(s.CreateBet)).Methods(http.MethodPost)
+	s.Router.HandleFunc("/create/bet", s.IfLoggedIn(s.CreateBet)).Methods(http.MethodPost)
 
 	s.Router.HandleFunc("/create/result", s.CheckIsAdmin(s.AddResult)).Methods(http.MethodPost)
 }
