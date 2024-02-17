091f08981d5d15d6a6d0e378d6c6a81b73728cc8
cosmicintruder <littlegreenbagg@googlemail.com>
2024-02-17 22:27:19 +0400

    added constants and token model comments

--- pkg/model/constants.go
+++ pkg/model/constants.go
@@ -1,12 +1,15 @@
 package model
 
+// Services names used in the application
 const (
 	AuthService   = "auth"
 	CommonService = "common"
 )
 
+// ContextField represents keys used to store values in the context.
 type ContextField string
 
+// Constants defining keys for commonly used context values.
 const (
 	ContextUserId          ContextField = "user_id"
 	ContextFlags           ContextField = "flags"
--- pkg/model/token.go
+++ pkg/model/token.go
@@ -1,9 +1,11 @@
 package model
 
+// TokenType represents the type for distinguishing token types in the application.
 type TokenType string
 
+// Constants for various token types.
 const (
-	TokenConfirmation  = "user_registration_confirmation"
-	TokenResetPassword = "reset_password"
-	TokenSetPassword   = "set_password"
+	TokenConfirmation  TokenType = "user_registration_confirmation"
+	TokenResetPassword TokenType = "reset_password"
+	TokenSetPassword   TokenType = "set_password"
 )
