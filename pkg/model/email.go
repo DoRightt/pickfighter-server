7655818ff004b6b50858a3c8528b1a9bea4e226e
cosmicintruder <littlegreenbagg@googlemail.com>
2024-02-17 22:12:21 +0400

    email models comments

--- pkg/model/email.go
+++ pkg/model/email.go
@@ -1,17 +1,20 @@
 package model
 
+// Email types used in the application.
 const (
 	EmailRegistration  = "registration"
 	EmailResetPassword = "reset_password"
 )
 
+// EmailAddrSpec represents an email address with an optional name.
 type EmailAddrSpec struct {
 	Email string `json:"email" yaml:"email"`
 	Name  string `json:"name" yaml:"name"`
 }
 
+// EmailData represents the data structure for sending emails in the application.
 type EmailData struct {
-	Sender    EmailAddrSpec `yaml:"sender" yaml:"sender"`
+	Sender    EmailAddrSpec `json:"sender" yaml:"sender"`
 	Recipient EmailAddrSpec `json:"recipient" yaml:"recipient"`
 	Subject   string        `json:"subject" yaml:"subject"`
 	Token     string        `json:"token" yaml:"token"`
