42525dbcb65a7ba486d8608b97c7921c8842d651
cosmicintruder <littlegreenbagg@googlemail.com>
2024-02-23 16:30:02 +0400

    added few mq-utils tests

---
+++ internal/services/middlewares_test.go
@@ -0,0 +1,15 @@
+package services
+
+import "testing"
+
+func TestVerifyJWT(t *testing.T) {
+	// TODO
+}
+
+func TestIfLoggedIn(t *testing.T) {
+	// TODO
+}
+
+func TestCheckIsAdmin(t *testing.T) {
+	// TODO
+}
