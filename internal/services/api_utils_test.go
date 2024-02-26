2c2b50b976f6ba68d980f8d0bc81c360731c3de7
cosmicintruder <littlegreenbagg@googlemail.com>
2024-02-26 18:21:55 +0400

    added additional test for HealthCheck

--- internal/services/api_utils_test.go
+++ internal/services/api_utils_test.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,10 +10,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type ApiServiceMock struct {
+	ServiceName string
+}
+
+func (m *ApiServiceMock) Init(ctx context.Context) error           { return nil }
+func (m *ApiServiceMock) ApplyRoutes()                             {}
+func (m *ApiServiceMock) Shutdown(ctx context.Context, sig string) {}
+
 func TestHealthCheck(t *testing.T) {
 	apiHandler := &ApiHandler{
 		ServiceName: "TestApp",
+		Services:    make(map[string]ApiService),
 	}
+
+	apiHandler.Services["TestA"] = &ApiServiceMock{ServiceName: "TestA"}
+	apiHandler.Services["TestB"] = &ApiServiceMock{ServiceName: "TestB"}
 
 	req, err := http.NewRequest("GET", "/health", nil)
 	assert.NoError(t, err)
@@ -19,5 +33,10 @@ func TestHealthCheck(t *testing.T) {
 	w := httptest.NewRecorder()
 	apiHandler.HealthCheck(w, req)
 
+	var response map[string]interface{}
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+
 	assert.Equal(t, http.StatusOK, w.Code)
+	assert.ElementsMatch(t, []interface{}{"TestA", "TestB"}, response["modules"])
 }
