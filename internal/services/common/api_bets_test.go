979aa942d313a9316622dea40f07de5f9a818f20
cosmicintruder <littlegreenbagg@googlemail.com>
2024-03-05 17:28:34 +0400

    api bets tests added

--- internal/services/common/api_bets_test.go
+++ internal/services/common/api_bets_test.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"projects/fb-server/internal/services"
 	"projects/fb-server/pkg/logger"
 	"projects/fb-server/pkg/model"
+	"strings"
 	"testing"
 	"time"
 
@@ -144,11 +146,128 @@ func TestGetBets(t *testing.T) {
 }
 
 func TestCreateBet(t *testing.T) {
-	// TODO
+	tests := []struct {
+		name           string
+		mockBehavior   func(ctx context.Context, mrepo *mock_repo.MockFbCommonRepo)
+		req            *http.Request
+		expectedStatus int
+	}{
+		{
+			name: "No ContextJWTPointer in context",
+			mockBehavior: func(ctx context.Context, mrepo *mock_repo.MockFbCommonRepo) {
+
+			},
+			req:            httptest.NewRequest("GET", "/example", nil),
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name: "No UserId in token",
+			mockBehavior: func(ctx context.Context, mrepo *mock_repo.MockFbCommonRepo) {
+
+			},
+			req: (func() *http.Request {
+				token, err := getFakeToken()
+				require.NoError(t, err)
+
+				bet := model.Bet{
+					BetId:     1,
+					FightId:   123,
+					UserId:    456,
+					FighterId: 789,
+				}
+
+				return createFakeRequestWithBody(token, bet)
+			})(),
+			expectedStatus: http.StatusUnauthorized,
+		},
+		{
+			name: "Success",
+			mockBehavior: func(ctx context.Context, mrepo *mock_repo.MockFbCommonRepo) {
+				mrepo.EXPECT().CreateBet(gomock.Any(), gomock.Any()).Return(int32(1), nil).Times(1)
+			},
+			req: (func() *http.Request {
+				token, err := getFakeToken()
+				require.NoError(t, err)
+
+				if err := token.Set(string(model.ContextUserId), float64(1)); err != nil {
+					log.Fatalf("Unable to set JWT token userRoles: %s", err)
+				}
+
+				bet := model.Bet{
+					BetId:     1,
+					FightId:   123,
+					UserId:    456,
+					FighterId: 789,
+				}
+
+				return createFakeRequestWithBody(token, bet)
+			})(),
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name: "CreateBet Error",
+			mockBehavior: func(ctx context.Context, mrepo *mock_repo.MockFbCommonRepo) {
+				mrepo.EXPECT().CreateBet(gomock.Any(), gomock.Any()).Return(int32(0), errors.New("some error")).Times(1)
+			},
+			req: (func() *http.Request {
+				token, err := getFakeToken()
+				require.NoError(t, err)
+
+				if err := token.Set(string(model.ContextUserId), float64(1)); err != nil {
+					log.Fatalf("Unable to set JWT token userRoles: %s", err)
+				}
+				return createFakeRequestWithToken(token)
+			})(),
+			expectedStatus: http.StatusBadRequest,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			ctx := context.Background()
+			mockRepo := mock_repo.NewMockFbCommonRepo(ctrl)
+
+			handler := &services.ApiHandler{
+				Logger: logger.NewSugared(),
+			}
+
+			service := &service{
+				Repo:       mockRepo,
+				ApiHandler: handler,
+			}
+
+			w := httptest.NewRecorder()
+
+			tc.mockBehavior(ctx, mockRepo)
+
+			service.CreateBet(w, tc.req)
+
+			assert.Equal(t, tc.expectedStatus, w.Code)
+		})
+	}
 }
 
 func createFakeRequestWithToken(token jwt.Token) *http.Request {
 	req := httptest.NewRequest("GET", "/example", nil)
+
+	ctx := context.WithValue(req.Context(), model.ContextJWTPointer, token)
+	req = req.WithContext(ctx)
+
+	return req
+}
+
+func createFakeRequestWithBody(token jwt.Token, body any) *http.Request {
+	b, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal("Error while marshal test")
+	}
+
+	bodyString := strings.NewReader(string(b))
+
+	req := httptest.NewRequest("POST", "/example", bodyString)
 
 	ctx := context.WithValue(req.Context(), model.ContextJWTPointer, token)
 	req = req.WithContext(ctx)
