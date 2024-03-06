34f2d908209f6d6685a3ee903af32afe046bb7e7
cosmicintruder <littlegreenbagg@googlemail.com>
2024-03-06 19:21:45 +0400

    added interface method

--- internal/services/common/api.go
+++ internal/services/common/api.go
@@ -14,6 +14,7 @@ import (
 type CommonService interface {
 	services.ApiService
 	CheckEventIsDone(ctx context.Context, tx pgx.Tx, fightId int32) error
+	HandleNewEvent(w http.ResponseWriter, r *http.Request)
 }
 
 type service struct {
