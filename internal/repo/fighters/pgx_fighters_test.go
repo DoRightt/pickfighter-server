4e974b7f3da4136164c7831dcbc1a8d70cbb89b0
cosmicintruder <littlegreenbagg@googlemail.com>
2024-03-21 18:51:11 +0400

    added test funcs placeholders

---
+++ internal/repo/fighters/pgx_fighters_test.go
@@ -0,0 +1,23 @@
+package repo
+
+import "testing"
+
+func TestFindFighter(t *testing.T) {
+	// TODO
+}
+
+func TestCreateNewFighter(t *testing.T) {
+	// TODO
+}
+
+func TestCreateNewFighterStats(t *testing.T) {
+	// TODO
+}
+
+func TestUpdateFighter(t *testing.T) {
+	// TODO
+}
+
+func TestUpdateFighterStats(t *testing.T) {
+	// TODO
+}
