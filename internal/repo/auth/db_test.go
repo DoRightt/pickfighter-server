71b2e1f24b3c8b4aeb6a9097948f4fd3996e6c89
cosmicintruder <littlegreenbagg@googlemail.com>
2024-03-22 13:34:49 +0400

    added repo credentials tests

--- internal/repo/auth/db_test.go
+++ internal/repo/auth/db_test.go
@@ -1,9 +1,16 @@
 package repo
 
 import (
+	"context"
+	"fmt"
+	"log"
 	mock_repo "projects/fb-server/internal/repo/auth/mocks"
+	"projects/fb-server/pkg/logger"
+	"projects/fb-server/pkg/pgxs"
+	"strings"
 	"testing"
 
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
@@ -24,4 +31,60 @@ func TestNew(t *testing.T) {
 	default:
 		t.Errorf("Expected Repo interface is FbAuthRepo, but got %v", v)
 	}
+}
+
+func updateTables() {
+	initTestConfig()
+
+	config := &pgxs.Config{
+		DataDir:  viper.GetString("postgres.test.data_dir"),
+		DbUri:    viper.GetString("postgres.test.url"),
+		Host:     viper.GetString("postgres.test.host"),
+		Port:     viper.GetString("postgres.test.port"),
+		Name:     viper.GetString("postgres.test.name"),
+		User:     viper.GetString("postgres.test.user"),
+		Password: viper.GetString("postgres.test.password"),
+	}
+
+	db, err := pgxs.NewPool(context.Background(), logger.NewSugared(), config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authRepo := New(db)
+
+	url := viper.GetString("postgres.test.url")
+	parts := strings.Split(url, "/")
+	dbName := parts[len(parts)-1]
+	testName := "test_table"
+
+	if dbName == testName {
+		_, err := authRepo.GetPool().Exec(context.Background(), "TRUNCATE TABLE public.fb_users, public.fb_user_credentials RESTART IDENTITY;")
+		fmt.Println("TRUNCATE")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func initTestConfig() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("../../../")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %s\n", err)
+	}
+}
+
+func getConfig() *pgxs.Config {
+	config := &pgxs.Config{
+		DataDir:  viper.GetString("postgres.test.data_dir"),
+		DbUri:    viper.GetString("postgres.test.url"),
+		Host:     viper.GetString("postgres.test.host"),
+		Port:     viper.GetString("postgres.test.port"),
+		Name:     viper.GetString("postgres.test.name"),
+		User:     viper.GetString("postgres.test.user"),
+		Password: viper.GetString("postgres.test.password"),
+	}
+
+	return config
 }
--- internal/repo/auth/pgx_user_test.go
+++ internal/repo/auth/pgx_user_test.go
@@ -2,12 +2,9 @@ package repo
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"projects/fb-server/pkg/logger"
 	"projects/fb-server/pkg/model"
 	"projects/fb-server/pkg/pgxs"
-	"strings"
 	"testing"
 
 	"github.com/jackc/pgx/v5"
@@ -15,40 +12,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"gopkg.in/go-playground/assert.v1"
 )
-
-func updateTables() {
-	initTestConfig()
-
-	config := &pgxs.Config{
-		DataDir:  viper.GetString("postgres.test.data_dir"),
-		DbUri:    viper.GetString("postgres.test.url"),
-		Host:     viper.GetString("postgres.test.host"),
-		Port:     viper.GetString("postgres.test.port"),
-		Name:     viper.GetString("postgres.test.name"),
-		User:     viper.GetString("postgres.test.user"),
-		Password: viper.GetString("postgres.test.password"),
-	}
-
-	db, err := pgxs.NewPool(context.Background(), logger.NewSugared(), config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	authRepo := New(db)
-
-	url := viper.GetString("postgres.test.url")
-	parts := strings.Split(url, "/")
-	dbName := parts[len(parts)-1]
-	testName := "test_table"
-
-	if dbName == testName {
-		_, err := authRepo.GetPool().Exec(context.Background(), "TRUNCATE TABLE public.fb_users RESTART IDENTITY;")
-		fmt.Println("TRUNCATE")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
 
 func TestTxCreateUser(t *testing.T) {
 	updateTables()
@@ -76,26 +39,4 @@ func TestTxCreateUser(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, userId, int32(1))
-}
-
-func initTestConfig() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath("../../../")
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file: %s\n", err)
-	}
-}
-
-func getConfig() *pgxs.Config {
-	config := &pgxs.Config{
-		DataDir:  viper.GetString("postgres.test.data_dir"),
-		DbUri:    viper.GetString("postgres.test.url"),
-		Host:     viper.GetString("postgres.test.host"),
-		Port:     viper.GetString("postgres.test.port"),
-		Name:     viper.GetString("postgres.test.name"),
-		User:     viper.GetString("postgres.test.user"),
-		Password: viper.GetString("postgres.test.password"),
-	}
-
-	return config
 }
