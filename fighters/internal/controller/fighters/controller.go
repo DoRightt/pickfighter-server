e821096e0140db0906591224760d4225d1273ef4
cosmicintruder <littlegreenbagg@googlemail.com>
2024-05-15 12:49:09 +0400

    fighters controller

---
+++ fighters/internal/controller/fighters/controller.go
@@ -0,0 +1,98 @@
+package fighters
+
+import (
+	"context"
+	"errors"
+
+	"fightbettr.com/fighters/pkg/logger"
+	"fightbettr.com/fighters/pkg/model"
+	"fightbettr.com/pkg/pgxs"
+	"github.com/jackc/pgx/v5"
+)
+
+// ErrNotFound is returned when a requested record is not found.
+var ErrNotFound = errors.New("not found")
+
+type fightersRepository interface {
+	pgxs.FbRepo
+	SearchFightersCount(ctx context.Context, req *model.FightersRequest) (int32, error)
+	SearchFighters(ctx context.Context, req *model.FightersRequest) ([]*model.Fighter, error)
+	FindFighter(ctx context.Context, req model.Fighter) (int32, error)
+	CreateNewFighter(ctx context.Context, tx pgx.Tx, fighter model.Fighter) (int32, error)
+	CreateNewFighterStats(ctx context.Context, tx pgx.Tx, stats model.FighterStats) error
+	UpdateFighter(ctx context.Context, tx pgx.Tx, fighter model.Fighter) (int32, error)
+	UpdateFighterStats(ctx context.Context, tx pgx.Tx, stats model.FighterStats) error
+}
+
+// Controller defines a metadata service controller.
+type Controller struct {
+	repo fightersRepository
+	// Router *mux.Router // DEL
+	Logger logger.FbLogger
+}
+
+// New creates a Fighters service controller.
+func New(repo fightersRepository) *Controller {
+	return &Controller{
+		repo: repo,
+		// Router: mux.NewRouter(), // DEL
+		Logger: logger.GetSugared(),
+	}
+}
+
+// SearchFightersCount retrieves the count of fighters based on the provided request.
+// It calls the repository's method and returns the count.
+// If an error occurs, it logs the error and returns it.
+func (c *Controller) SearchFightersCount(ctx context.Context, req *model.FightersRequest) (int32, error) {
+	count, err := c.repo.SearchFightersCount(ctx, req)
+	if err != nil {
+		c.Logger.Errorf("Failed to get fighters count: %s", err)
+
+		// TODO errors package for grpc
+		// httplib.ErrorResponseJSON(w, http.StatusInternalServerError, internalErr.CountFighters, err)
+
+		return 0, err
+	}
+
+	return count, nil
+}
+
+// SearchFighters retrieves fighters based on the provided request.
+// It first retrieves the count of fighters to determine if any exist.
+// If no fighters are found, it returns an empty list.
+// Otherwise, it calls the repository's method to fetch the fighters and returns them.
+// If an error occurs during the process, it logs the error and returns it.
+func (c *Controller) SearchFighters(ctx context.Context, req *model.FightersRequest) ([]*model.Fighter, error) {
+	count, err := c.repo.SearchFightersCount(ctx, req)
+	if err != nil {
+		c.Logger.Errorf("Failed to get fighters count: %s", err)
+
+		// TODO errors package for grpc
+		// httplib.ErrorResponseJSON(w, http.StatusInternalServerError, internalErr.CountFighters, err)
+
+		return []*model.Fighter{}, err
+	}
+
+	if count == 0 {
+		return []*model.Fighter{}, nil
+	}
+
+	fighters, err := c.repo.SearchFighters(ctx, req)
+	if err != nil {
+		c.Logger.Errorf("Failed to find fighters: %s", err)
+
+		// TODO errors package for grpc
+		// httplib.ErrorResponseJSON(w, http.StatusInternalServerError, internalErr.Fighters, err)
+
+		return []*model.Fighter{}, err
+	}
+
+	return fighters, nil
+}
+
+func (c *Controller) GracefulShutdown(ctx context.Context, sig string) {
+	c.Logger.Warnf("Graceful shutdown. Signal received: %s", sig)
+	if c.repo != nil {
+		c.repo.GracefulShutdown()
+	}
+}
