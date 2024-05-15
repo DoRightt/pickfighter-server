1cef37317ff5a593be2e91d2e4d3680d4b3945bb
cosmicintruder <littlegreenbagg@googlemail.com>
2024-05-15 12:54:41 +0400

    repo methods for grpc api

---
+++ fighters/internal/repository/psql/db.go
@@ -0,0 +1,27 @@
+package psql
+
+import (
+	"context"
+
+	"fightbettr.com/fighters/pkg/cfg"
+	"fightbettr.com/fighters/pkg/logger"
+	"fightbettr.com/pkg/pgxs"
+)
+
+// Repository represents a repository for interacting with fighter-related data in the database.
+// It embeds the pgxs.Repo, which provides the basic PostgreSQL database operations.
+type Repository struct {
+	pgxs.FbRepo
+}
+
+// New creates and returns a new instance of Fighters Repository using the provided logger
+func New(ctx context.Context, logger logger.FbLogger) (*Repository, error) {
+	db, err := pgxs.NewPool(ctx, logger, cfg.ViperPostgres())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Repository{
+		FbRepo: db,
+	}, nil
+}
