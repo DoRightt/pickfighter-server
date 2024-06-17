1b47bc6012993c53d0dc578143e8ee25e5dedc15
cosmicintruder <littlegreenbagg@googlemail.com>
2024-06-17 22:52:03 +0400

    fightbettr controller changes

--- fightbettr/internal/controller/fightbettr/controller.go
+++ fightbettr/internal/controller/fightbettr/controller.go
@@ -3,6 +3,7 @@ package fightbettr
 import (
 	"context"
 
+	authmodel "fightbettr.com/auth/pkg/model"
 	fightersmodel "fightbettr.com/fighters/pkg/model"
 )
 
@@ -10,13 +11,24 @@ type fightersGateway interface {
 	SearchFighters(ctx context.Context, status fightersmodel.FighterStatus) ([]*fightersmodel.Fighter, error)
 }
 
+type authGateway interface {
+	Register(ctx context.Context, req *authmodel.RegisterRequest) (*authmodel.UserCredentials, error)
+	ConfirmRegistration(ctx context.Context, token string) (bool, error)
+	Login(ctx context.Context, req *authmodel.AuthenticateRequest) (*authmodel.AuthenticateResult, error)
+	ResetPassword(ctx context.Context, req *authmodel.ResetPasswordRequest) (bool, error)
+	PasswordRecover(ctx context.Context, req *authmodel.RecoverPasswordRequest) (bool, error)
+	GetCurrentUser(ctx context.Context) (*authmodel.User, error)
+}
+
 // Controller defines a gateway service controller.
 type Controller struct {
+	authGateway     authGateway
 	fightersGateway fightersGateway
 }
 
-func New(fightersGateway fightersGateway) *Controller {
+func New(authGateway authGateway, fightersGateway fightersGateway) *Controller {
 	return &Controller{
+		authGateway,
 		fightersGateway,
 	}
 }
@@ -29,4 +41,58 @@ func (c *Controller) SearchFighters(ctx context.Context, status string) ([]*figh
 	}
 
 	return fighters, nil
+}
+
+func (c *Controller) Register(ctx context.Context, req *authmodel.RegisterRequest) (*authmodel.UserCredentials, error) {
+	credentials, err := c.authGateway.Register(ctx, req)
+	if err != nil {
+		return &authmodel.UserCredentials{}, err
+	}
+
+	return credentials, nil
+}
+
+func (c *Controller) ConfirmRegistration(ctx context.Context, token string) (bool, error) {
+	ok, err := c.authGateway.ConfirmRegistration(ctx, token)
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}
+
+func (c *Controller) Login(ctx context.Context, req *authmodel.AuthenticateRequest) (*authmodel.AuthenticateResult, error) {
+	token, err := c.authGateway.Login(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func (c *Controller) ResetPassword(ctx context.Context, req *authmodel.ResetPasswordRequest) (bool, error) {
+	ok, err := c.authGateway.ResetPassword(ctx, req)
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}
+
+func (c *Controller) PasswordRecover(ctx context.Context, req *authmodel.RecoverPasswordRequest) (bool, error) {
+	ok, err := c.authGateway.PasswordRecover(ctx, req)
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}
+
+func (c *Controller) GetCurrentUser(ctx context.Context) (*authmodel.User, error) {
+	user, err := c.authGateway.GetCurrentUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
