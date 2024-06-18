00d2ba00d854ec62f3416af80136a7b1397d10d6
cosmicintruder <littlegreenbagg@googlemail.com>
2024-06-18 22:21:35 +0400

    fightbettr controller comments

--- fightbettr/internal/controller/fightbettr/controller.go
+++ fightbettr/internal/controller/fightbettr/controller.go
@@ -26,6 +26,7 @@ type Controller struct {
 	fightersGateway fightersGateway
 }
 
+// New creates new Controller instance
 func New(authGateway authGateway, fightersGateway fightersGateway) *Controller {
 	return &Controller{
 		authGateway,
@@ -43,6 +44,8 @@ func (c *Controller) SearchFighters(ctx context.Context, status string) ([]*figh
 	return fighters, nil
 }
 
+// Register handles the registration of a new user. It takes a context and a 
+// RegisterRequest, and returns the registered UserCredentials or an error.
 func (c *Controller) Register(ctx context.Context, req *authmodel.RegisterRequest) (*authmodel.UserCredentials, error) {
 	credentials, err := c.authGateway.Register(ctx, req)
 	if err != nil {
@@ -52,6 +55,7 @@ func (c *Controller) Register(ctx context.Context, req *authmodel.RegisterReques
 	return credentials, nil
 }
 
+// ConfirmRegistration confirms a user's registration with the provided token.
 func (c *Controller) ConfirmRegistration(ctx context.Context, token string) (bool, error) {
 	ok, err := c.authGateway.ConfirmRegistration(ctx, token)
 	if err != nil {
@@ -61,6 +65,7 @@ func (c *Controller) ConfirmRegistration(ctx context.Context, token string) (boo
 	return ok, nil
 }
 
+// // Login authenticates a user with the provided credentials.
 func (c *Controller) Login(ctx context.Context, req *authmodel.AuthenticateRequest) (*authmodel.AuthenticateResult, error) {
 	token, err := c.authGateway.Login(ctx, req)
 	if err != nil {
@@ -70,6 +75,7 @@ func (c *Controller) Login(ctx context.Context, req *authmodel.AuthenticateReque
 	return token, nil
 }
 
+// ResetPassword resets a user's password with the provided request details.
 func (c *Controller) ResetPassword(ctx context.Context, req *authmodel.ResetPasswordRequest) (bool, error) {
 	ok, err := c.authGateway.ResetPassword(ctx, req)
 	if err != nil {
@@ -79,6 +85,7 @@ func (c *Controller) ResetPassword(ctx context.Context, req *authmodel.ResetPass
 	return ok, nil
 }
 
+// PasswordRecover initiates the password recovery process for a user with the provided request details.
 func (c *Controller) PasswordRecover(ctx context.Context, req *authmodel.RecoverPasswordRequest) (bool, error) {
 	ok, err := c.authGateway.PasswordRecover(ctx, req)
 	if err != nil {
@@ -88,6 +95,7 @@ func (c *Controller) PasswordRecover(ctx context.Context, req *authmodel.Recover
 	return ok, nil
 }
 
+// GetCurrentUser retrieves the currently authenticated user.
 func (c *Controller) GetCurrentUser(ctx context.Context) (*authmodel.User, error) {
 	user, err := c.authGateway.GetCurrentUser(ctx)
 	if err != nil {
