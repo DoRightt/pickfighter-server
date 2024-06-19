50b141a186edbfc7aa23533d0813a8c34b1f9601
cosmicintruder <littlegreenbagg@googlemail.com>
2024-06-19 18:32:35 +0400

    repo package

---
+++ events/internal/repository/psql/db.go
@@ -0,0 +1,33 @@
+package psql
+
+import (
+	"context"
+
+	"fightbettr.com/events/pkg/cfg"
+	"fightbettr.com/events/pkg/logger"
+	"fightbettr.com/pkg/pgxs"
+)
+
+const sep = ` AND `
+
+// Repository represents a repository for interacting with user data in the database.
+// It embeds the pgxs.Repo, which provides the basic PostgreSQL database operations.
+type Repository struct {
+	pgxs.FbRepo
+}
+
+// New creates and returns a new instance of Repository using the provided logger
+func New(ctx context.Context, logger logger.FbLogger) (*Repository, error) {
+	db, err := pgxs.NewPool(ctx, logger, cfg.ViperPostgres())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Repository{
+		FbRepo: db,
+	}, nil
+}
+
+func (r *Repository) PoolClose() {
+	r.GetPool().Close()
+}
