9bece079d920de7c057936fb8dd21edaa5b8adb4
cosmicintruder <littlegreenbagg@googlemail.com>
2024-06-24 18:03:10 +0400

    constant for fightbettr service

--- fightbettr/pkg/model/constants.go
+++ fightbettr/pkg/model/constants.go
@@ -4,3 +4,11 @@ package model
 const (
 	GatewayService = "fightbettr"
 )
+
+// ContextField represents keys used to store values in the context.
+type ContextField string
+
+// Constants defining keys for commonly used context values.
+const (
+	ContextUserId ContextField = "user_id"
+)
