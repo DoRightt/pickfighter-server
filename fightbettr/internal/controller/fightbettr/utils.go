1593f9d9a756035b81778c3fef13aca14af4e2b2
cosmicintruder <littlegreenbagg@googlemail.com>
2024-07-06 12:03:03 +0400

    gateway controller utils

---
+++ fightbettr/internal/controller/fightbettr/utils.go
@@ -0,0 +1,55 @@
+package fightbettr
+
+import (
+	eventmodel "fightbettr.com/events/pkg/model"
+	gatewaymodel "fightbettr.com/fightbettr/pkg/model"
+	fightersmodel "fightbettr.com/fighters/pkg/model"
+)
+
+func (c *Controller) getFightersIds(events []*eventmodel.Event) []int32 {
+	var ids []int32
+	uniqueIdsMap := make(map[int32]struct{})
+
+	for _, event := range events {
+		fights := event.Fights
+
+		for _, fight := range fights {
+			redId := fight.FighterRedId
+			blueId := fight.FighterBlueId
+
+			if _, exists := uniqueIdsMap[redId]; !exists {
+				uniqueIdsMap[redId] = struct{}{}
+				ids = append(ids, redId)
+			}
+
+			if _, exists := uniqueIdsMap[blueId]; !exists {
+				uniqueIdsMap[blueId] = struct{}{}
+				ids = append(ids, blueId)
+			}
+		}
+	}
+
+	return ids
+}
+
+func (c *Controller) getFightersList(fighters []*fightersmodel.Fighter) map[int32]*fightersmodel.Fighter {
+	list := make(map[int32]*fightersmodel.Fighter)
+
+	for _, fighter := range fighters {
+		list[fighter.FighterId] = fighter
+	}
+
+	return list
+}
+
+func (c *Controller) eventsPretify(events []*eventmodel.Event, fighters []*fightersmodel.Fighter) []*gatewaymodel.Event {
+	fightersList := c.getFightersList(fighters)
+	updatedEvents := make([]*gatewaymodel.Event, len(events))
+
+	for i, v := range events {
+		event := gatewaymodel.ServiceEventToGatewayEvent(v, fightersList)
+		updatedEvents[i] = event
+	}
+
+	return updatedEvents
+}
