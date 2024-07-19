5488cb562c000e57f8f8efa9be1bf58d111117ee
cosmicintruder <littlegreenbagg@googlemail.com>
2024-07-19 16:39:16 +0400

    added test for pkg cfg

--- fighters/pkg/cfg/postgres_test.go
+++ fighters/pkg/cfg/postgres_test.go
@@ -26,3 +26,23 @@ func TestViperPostgres(t *testing.T) {
 	assert.Equal(t, "test_user", cfg.User)
 	assert.Equal(t, "test_password", cfg.Password)
 }
+
+func TestViperTestPostgres(t *testing.T) {
+	viper.Set("postgres.test.data_dir", "/test/data_dir")
+	viper.Set("postgres.test.url", "postgres://localhost/test")
+	viper.Set("postgres.test.host", "localhost")
+	viper.Set("postgres.test.port", "5432")
+	viper.Set("postgres.test.name", "test")
+	viper.Set("postgres.test.user", "test_user")
+	viper.Set("postgres.test.password", "test_password")
+
+	cfg := ViperTestPostgres()
+
+	assert.Equal(t, "/test/data_dir", cfg.DataDir)
+	assert.Equal(t, "postgres://localhost/test", cfg.DbUri)
+	assert.Equal(t, "localhost", cfg.Host)
+	assert.Equal(t, "5432", cfg.Port)
+	assert.Equal(t, "test", cfg.Name)
+	assert.Equal(t, "test_user", cfg.User)
+	assert.Equal(t, "test_password", cfg.Password)
+}
