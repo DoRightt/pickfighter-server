5792722bf3ff9b952bd539f44caec68c684c078c
cosmicintruder <littlegreenbagg@googlemail.com>
2025-01-16 18:55:16 +0400

    change fighters service scheme

--- events/internal/repository/psql/db_fights.go
+++ events/internal/repository/psql/db_fights.go
@@ -7,12 +7,12 @@ import (
 	eventmodel "pickfighter.com/events/pkg/model"
 )
 
-// TxCreateEventFight creates a new fight in the 'pf_fights' table within a transaction.
+// TxCreateEventFight creates a new fight in the 'fights' table within a transaction.
 // It takes a context, a transaction, and a Fight model.
 // It returns an error if the insertion fails.
 func (r *Repository) TxCreateEventFight(ctx context.Context, tx pgx.Tx, f eventmodel.Fight) error {
 	q := `INSERT INTO
-		public.pf_fights(event_id, fighter_red_id, fighter_blue_id, is_done, not_contest)
+		events.fights(event_id, fighter_red_id, fighter_blue_id, is_done, not_contest)
 		VALUES ($1, $2, $3, $4, $5)`
 
 	args := []any{
@@ -32,11 +32,11 @@ func (r *Repository) TxCreateEventFight(ctx context.Context, tx pgx.Tx, f eventm
 	return nil
 }
 
-// SetFightResult updates the result of a fight in the 'pf_fights' table.
+// SetFightResult updates the result of a fight in the 'fights' table.
 // It takes a context, a transaction, and a FightResultRequest.
 // It returns an error if the update fails.
 func (r *Repository) SetFightResult(ctx context.Context, tx pgx.Tx, req *eventmodel.FightResultRequest) error {
-	q := `UPDATE pf_fights
+	q := `UPDATE events.fights
 	SET result = $1, not_contest = $2, is_done = true
 	WHERE fight_id = $3;`
 
