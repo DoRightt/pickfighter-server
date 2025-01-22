222b5a5067f01e7ef37fc166134e86cc0483fe6e
cosmicintruder <littlegreenbagg@googlemail.com>
2025-01-22 10:25:19 +0400

    change back file path

--- fighters/cmd/utils.go
+++ fighters/cmd/utils.go
@@ -19,9 +19,10 @@ import (
 )
 
 // ReadFighterData reads fighter data from a JSON file and returns a slice of model.Fighter.
-// The file path is set to "../scraper/collection/fighters.json".
+// The file path is set to "../../scraper/collection/fighters.json".
 func ReadFighterData() ([]model.Fighter, error) {
-	filePath := "../scraper/collection/fighters.json"
+	// TODO: tricky path
+	filePath := "../../scraper/collection/fighters.json"
 
 	jsonData, err := os.ReadFile(filePath)
 	if err != nil {
